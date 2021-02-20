18d4c7e4a7f76ce51a9a1bfde45c38d1f0f0486b
mattia <mattia.mancarella.88@gmail.com>
2021-02-20 22:41:11 +0100

    new: implementato costruttore api aqicn + calcolo precisione risultati

---
+++ src/js/modules/AqicnApi.js
@@ -0,0 +1,135 @@
+'use strict';
+
+/**
+ * @class
+ * @param {string} apiKey - chiave di accesso personale
+ */
+function AqicnApi(apiKey) {
+    this._apiKey = apiKey;
+    this._cache = new Map();
+    this._controller = null;
+    this._baseUrl = 'https://api.waqi.info/feed/';
+
+    /**
+     * Invia richiesta GET verso api
+     * 
+     * @private
+     * @function
+     * @param {string} _path - parametri da inserire nel path dell'url
+     * @returns {Promise}
+     */
+    this._ApiCall = async function (path) {
+        // se la chiave non è valorizzata creo l'errore
+        if (this._apiKey === null) throw {
+            name: "InvalidApiKey",
+            message: "'null' isn't a valid key"
+        };
+
+        if (this._controller) this._controller.abort();
+
+        this._controller = new AbortController();
+
+        const apiKey = `token=${this._apiKey}`;
+        let apiUrl = [this._baseUrl, path, '/', '?', apiKey].join('');
+        let response = [];
+
+        try {
+            // codifico url
+            apiUrl = encodeURI(apiUrl);
+            // se ho già il risultato di questa query in cache lo ritorno
+            if (this._cache.has(apiUrl))
+                response = this._cache.get(apiUrl);
+            else {
+                // altrimenti invio chiamata GET all'api
+                response = await fetch(apiUrl, {
+                    signal: this._controller.signal
+                });
+                // salvo risposta nella cache
+                this._cache.set(apiUrl, response);
+            }
+            if (response.ok) return await response.clone().json();
+            else {
+                throw {
+                    name: "AqicnError",
+                    message: `Richiesta fallita (code: ${response.status})`
+                }
+            }
+        } catch (e) {
+            // questo errore viene chiamato ogni volta che il controller annulla la chiamata
+            if (e instanceof DOMException) {
+                return [];
+            }
+            throw e;
+        }
+    };
+
+    /**
+     * date latitudine e longitudine, ritorna le rilevazioni della stazione più vicina
+     * 
+     * @function
+     * @param  {(string|number)} lat - latitudine
+     * @param  {(string|number)} lng - longitudine
+     * @returns {Array}
+     */
+    this.GeolocalizedFeed = async function(lat, lng) {
+
+        const path = `geo:${lat};${lng}`;
+        let result = [];
+        
+        try {
+            result = await this._ApiCall(path);
+        } catch (e) {
+            throw e;
+        }
+
+        if (result.status === 'ok') {
+            // misuro distanza tra coordinate per ricavare precisione
+            const [station_lat, station_lng] = result.data.city.geo;
+            result.data.precision = this._calcCrow(lat, lng, station_lat, station_lng);
+            return result.data;
+        }
+        else 
+            throw {name:'AqicnApiError', message: result.data};
+    }
+
+    /**
+     * This function takes in latitude and longitude of two location and returns the distance between them as the crow flies( in km)
+     * 
+     * @private
+     * @function
+     * @param {number} lat1 
+     * @param {number} lon1 
+     * @param {number} lat2 
+     * @param {number} lon2 
+     * @returns {number}
+     */
+    this._calcCrow = function (lat1, lon1, lat2, lon2) {
+        var R = 6371; // km
+        var dLat = this._toRad(lat2 - lat1);
+        var dLon = this._toRad(lon2 - lon1);
+        var lat1 = this._toRad(lat1);
+        var lat2 = this._toRad(lat2);
+
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        return d;
+    }
+
+
+    /**
+     * Converts numeric degrees to radians
+     * 
+     * @private
+     * @function
+     * @param {number} Value 
+     * @returns {number}
+     */
+    this._toRad = function(Value) {
+        return Value * Math.PI / 180;
+    }
+
+}
+
+export default AqicnApi;
